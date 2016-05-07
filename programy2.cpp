cdf8bbb6508a4d0625cb5336de5cd4064dc88f40
Alphacentauri01 <djglober@wp.pl>
2016-05-07 16:45:16 +0200

    Update programy2.cpp

--- programy2.cpp
+++ programy2.cpp
@@ -4,7 +4,7 @@ int main()
 	int a,b; double c=5;
 	printf("Podaj dwie liczby: a i nacisnij enter i b i nacisnij enter:");
 	scanf("%d %d", &a,&b);
-	printf("a= %b b= %d\n",a,b);
+	printf("a= %d b= %d\n",a,b);
 	printf("suma= %d\n",a*b);
 	printf("suma= %d\n",a-b);
 	printf("%d / lf = %lf\n",a,c,a/c);
