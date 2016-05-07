b84a33b72741b3855f484dcbaf637db3f6ec2225
Alphacentauri01 <djglober@wp.pl>
2016-05-07 16:44:28 +0200

    Update programy2.cpp

--- programy2.cpp
+++ programy2.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
-int main()
+int main() 
 {
 	int a,b; double c=5;
 	printf("Podaj dwie liczby: a i nacisnij enter i b i nacisnij enter:");
 	scanf("%d %d", &a,&b);
 	printf("a=%b b=%d\n",a,b);
-	printf ("suma=%d\n",a*b);
+	printf("suma=%d\n",a*b);
 	printf("suma=%d\n",a-b);
 	printf("%d / lf = %lf\n",a,c,a/c);
 	return 0;
